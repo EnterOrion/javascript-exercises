f8ba6bf1f1b5657647b5e710370c3df40e06d5f2
EnterOrion <orionwesson@gmail.com>
2022-11-01 13:22:34 -0300

    Final solution complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let contString = "";
 
+    do {
+        contString += string.slice(-1);
+        string = string.slice(0, -1);
+    }
+    while (string.length > 0)
+    
+    return contString;
 };
+
+reverseString("hello");
+
 
 // Do not edit below this line
 module.exports = reverseString;
