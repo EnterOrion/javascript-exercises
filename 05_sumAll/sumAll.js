5f16654936268ba3040328d18ec1f157ddda9917
EnterOrion <orionwesson@gmail.com>
2022-11-01 15:43:59 -0300

    Final solution complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (isNaN(start) == true || isNaN(end) == true) {
+        return "ERROR"
+    }
+    else if (typeof start == "string" || typeof end == "string") {
+        return "ERROR"
+    }
+    else if (start < 0 || end < 0) {
+        return "ERROR"
+    }
 
+    if (start > end) {
+        let x = start; 
+        let y = end;
+        start = y;
+        end = x;
+    }
+    
+    let sum = 0;
+    let j = 0;
+    let i = 0;
+    while (j < end) {
+        sum += start + i;
+        i++;
+        j++;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
