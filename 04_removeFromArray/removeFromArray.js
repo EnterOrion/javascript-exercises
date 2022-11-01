2109fe0203137363404edd173fd0a7fc3b591e65
EnterOrion <orionwesson@gmail.com>
2022-11-01 15:21:17 -0300

    Final solution complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args[0];
+    for (let j=0; j<args.length; j++) {
+        for (let i=0; i<array.length; i++) {
+            if (array[i] === args[j]) {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
+}
+    
 
-};
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
