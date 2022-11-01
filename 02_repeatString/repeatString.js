e446d3896ca6a33367cd81a5e48e51dc9fc5d788
EnterOrion <orionwesson@gmail.com>
2022-10-31 21:46:45 -0300

    Final solution complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeats) {
+    if (numRepeats < 0) {
+        return "ERROR"
+    }
+    else if (numRepeats == 0) {
+        return ""
+    }
+    let newString = string;
+    
+    for (let i = 1; i < numRepeats; i++) {
+        newString += string;
+    }
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
