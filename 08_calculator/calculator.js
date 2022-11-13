4221386df6778c965a6577e0bdbbcc23249459a5
EnterOrion <orionwesson@gmail.com>
2022-11-13 14:01:04 -0300

    Final solution complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiFinal = 1;
+  if (array.length == 0) {
+    return 0
+  }
+  for (let i = 0; i < array.length; i++) {
+    multiFinal = multiFinal * array[i];
+  }
+  return multiFinal;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a == 0 || a == 1) {
+    return 1;
+  }
+  else {
+    return a * factorial(a -1);
+  }
 	
 };
 
