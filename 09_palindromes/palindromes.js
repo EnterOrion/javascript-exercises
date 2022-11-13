c3874c2858d6f120f34a5634d7d7a180cff7aed6
EnterOrion <orionwesson@gmail.com>
2022-11-13 14:36:29 -0300

    Final solution complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = '!?.,'
+    let cleanString = string;
+    cleanString = 
+    cleanString
+    .split('')
+    .filter(function(letter) {
+      return punctuation.indexOf(letter) === -1;
+    })
+    .join('');
+    cleanString = cleanString.split(' ').join('');
+    cleanString = cleanString.toLowerCase();
+    
+    let reverseString = cleanString.split('');
+    reverseString = reverseString.reverse();
+    reverseString = reverseString.join('');
 
+    if (reverseString == cleanString) {
+        return true;
+    }
+    return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
